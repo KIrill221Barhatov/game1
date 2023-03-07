a1d2608dd4e5d8d28e5c39699490f3a857ccfe07
Кирилл Бархатов <117072370+KIrill221Barhatov@users.noreply.github.com>
2023-03-07 13:54:09 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,24 @@
+var character = document.getElementById("character");
+
+document.addEventListener("click", function(event) {
+    jump ();
+})
+ 
+function jump () {
+    if (character.classList != 'animate') {
+        character.classList.add('animate')
+    }
+    setTimeout(function(){
+        character.classList.remove('animate')
+    }, 300)
+} 
+let isAlive = setInterval (function () {
+    let dino = parseInt(window.getComputedStyle(character).getPropertyValue('top'))
+    let cactus = parseInt(window.getComputedStyle(block).getPropertyValue('left'))
+    if (cactus < 50 && cactus > 0 && dino >= 140){
+    alert('GAME OVER')
+}
+}, 10)
+
+
+
